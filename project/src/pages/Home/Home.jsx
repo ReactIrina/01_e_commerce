4aa0b33592fc3c60212e73f5fa07329b72589f26
Irina Spasova <irina.spasova.81@gmail.com>
2024-06-11 12:44:47 +0300

    Card component

--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 //import React from "react";
 import Banner from "./Banner";
 import Companies from "./Companies";
+import Products from "./Products";
 
 const Home = () => {
   return (
     <div className="max-w-screen-2xl container mx-auto ">
       <Banner />
       <Companies className="w-full mx-auto" />
+      <Products />
     </div>
   );
 };
